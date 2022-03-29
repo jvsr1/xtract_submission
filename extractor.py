1dd7c4416072155d0f2ef09dba4a85370c06dfc9
Joao Victor Rodrigues <joaovictorsr2001@gmail.com>
2022-03-29 14:20:09 -0500

    first commit

--- extractor.py
+++ extractor.py
@@ -1,12 +1,18 @@
 """
-Create your own extractors, and package them below!
+Create your own extractor subcomponents, following the model below. 
+Subcomponents should be independent and not produce side-effects in
+the input.
 """
 def get_file_contents(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
 
+
 def execute_extractor(file_path):
+    """ The general model of extraction is for the method `execute_extractor` 
+    to call extractor subcomponents and store the results in a dictionary.
+    """
     mdata = {
         'contents': get_file_contents(file_path),
     }
